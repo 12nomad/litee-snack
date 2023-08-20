21d931ef161b27cecce99156ae31ca168820a638
Jo Harijaonina <jo.harijaonina@gmail.com>
2023-08-20 11:21:31 +0300

    fix config module not referecing env file in production

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,6 +1,6 @@
 import { PrismaClient } from '@prisma/client';
-import { faker } from '@faker-js/faker';
 import * as argon from 'argon2';
+import { faker } from '@faker-js/faker';
 
 const prisma = new PrismaClient();
 
